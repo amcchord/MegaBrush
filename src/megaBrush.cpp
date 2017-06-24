65c38b269e0c23a1b70fb849147256b0c3d64cf5
Austin McChord <amcchord+backupify@dattobackup.com>
2017-06-23 20:36:29 -0400

    Working PWM test

--- src/megaBrush.cpp
+++ src/megaBrush.cpp
@@ -24,6 +24,24 @@ THE SOFTWARE.
 
 #include <Arduino.h>
 
+
+#include <SoftPWM.h>
+
+/* pins_arduino.h defines the pin-port/bit mapping as PROGMEM so
+   you have to read them with pgm_read_xxx(). That's generally okay
+   for ordinary use, but really bad when you're writing super fast
+   codes because the compiler doesn't treat them as constants and
+   cannot optimize them away with sbi/cbi instructions.
+
+   Therefore we have to tell the compiler the PORT and BIT here.
+   Hope someday we can find a way to workaround this.
+
+   Check the manual of your MCU for port/bit mapping.
+
+   The following example demonstrates setting channels for all pins
+   on the ATmega328P or ATmega168 used on Arduino Uno, Pro Mini,
+   Nano and other boards. */
+
 #define rcIN 12
 #define ApFET 2
 #define AnFET 13 //PWM
@@ -35,10 +53,25 @@ THE SOFTWARE.
 #define CnFET 5 //PWM
 
 
+int x = 0;
+bool up = true;
+
+// SOFTPWM_DEFINE_CHANNEL(2, DDRD, PORTD, PORTD2);  //Arduino pin 2
+// SOFTPWM_DEFINE_CHANNEL_INVERT(3, DDRD, PORTD, PORTD3);  //Arduino pin 3
+// SOFTPWM_DEFINE_CHANNEL(4, DDRD, PORTD, PORTD4);  //Arduino pin 4
+SOFTPWM_DEFINE_CHANNEL(5, DDRD, PORTD, PORTD5);  //Arduino pin 5
+// SOFTPWM_DEFINE_CHANNEL(7, DDRD, PORTD, PORTD7);  //Arduino pin 7
+// SOFTPWM_DEFINE_CHANNEL(13, DDRB, PORTB, PORTB5);  //Arduino pin 13
+ SOFTPWM_DEFINE_OBJECT_WITH_PWM_LEVELS(6, 250);
+ SOFTPWM_DEFINE_EXTERN_OBJECT_WITH_PWM_LEVELS(6, 250);
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
+
+  Palatis::SoftPWM.begin(400);
+
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(ApFET, OUTPUT);
   pinMode(AnFET, OUTPUT);
@@ -47,26 +80,46 @@ void setup() {
   pinMode(CpFET, OUTPUT);
   pinMode(CnFET, OUTPUT);
 
-  digitalWrite(ApFET, HIGH);
+  digitalWrite(ApFET, HIGH); //Shutdown the highside
   digitalWrite(BpFET, HIGH);
   digitalWrite(CpFET, HIGH);
 
-  delay(4000);
+  delay(1000);
+  digitalWrite(BpFET,LOW);
+  digitalWrite(CnFET, HIGH);
+
+  delay(1000);
+  digitalWrite(BpFET,HIGH);
+  digitalWrite(CnFET, LOW);
+
+  delay(1000);
 
 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    digitalWrite(4,HIGH);
-    digitalWrite(7,LOW);
 
-    delay(1);
+    digitalWrite(BpFET,LOW);
+    Palatis::SoftPWM.set(5,x);
+    //digitalWrite(CnFET, HIGH);
 
-    digitalWrite(4,LOW);
-    digitalWrite(7,HIGH);
+    if (x > 254 && up == true){
+      up = false;
+      x--;
+    }
+    if (x < 1 && up == false){
+      up = true;
+      x++;
+    }
 
-    delay(1);
+    if (up){
+      x++;
+    } else {
+      x--;
+    }
+
+    delay(10);
    // tone(7,400,100);
 
 
